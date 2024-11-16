20d127d5bd620039b7019425674a0626871ae5fd
Agate Business Consulting <agate.biz.consulting@gmail.com>
2024-11-17 01:12:43 +0530

    Changes to key notification files
    Changes to key notification files - with the he;p of blackbox AI Chat

--- lib/core/notification/push_notifications.dart
+++ lib/core/notification/push_notifications.dart
@@ -1,78 +1,70 @@
-// import 'dart:convert';
-// import 'package:Yes_Loyalty/core/notification/local_notification.dart';
-// import 'package:firebase_messaging/firebase_messaging.dart';
-// import 'package:flutter/material.dart';
+import 'dart:convert';
+import 'package:Yes_Loyalty/core/notification/local_notification.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:flutter/material.dart';
 
-// class PushNotificationsService {
-//   //create an instance of the firebase messaging plugin
-//   static final _firebaseMessaging = FirebaseMessaging.instance;
+class PushNotificationsService {
+  // Create an instance of the Firebase Messaging plugin
+  static final FirebaseMessaging _firebaseMessaging = FirebaseMessaging.instance;
 
-//   //initialize the firebase messaging (request permission for notifications)
+  // Initialize Firebase Messaging and request permission for notifications
+  static Future<void> init() async {
+    // Request permission for notifications
+    NotificationSettings settings = await _firebaseMessaging.requestPermission(
+      alert: true,
+      badge: true,
+      provisional: false,
+      sound: true,
+    );
 
-//   static Future<void> init() async {
-//     //request permission for notifications
-//     NotificationSettings settings = await _firebaseMessaging.requestPermission(
-//       alert: true,
-//       badge: true,
-//       provisional: false,
-//       sound: true,
-//     );
+    // Check if the permission is granted
+    if (settings.authorizationStatus == AuthorizationStatus.authorized) {
+      print('User  granted permission');
+    } else {
+      print('User  declined or has not accepted permission');
+    }
 
-//     //check if the permission is granted
-//     if (settings.authorizationStatus == AuthorizationStatus.authorized) {
-//       print('User granted permission');
-//     } else {
-//       print('User declined or has not accepted permission');
-//     }
+    // Get the FCM (Firebase Cloud Messaging) token from the device
+    try {
+        String? token = await _firebaseMessaging.getToken();
+        print('FCM Token: $token');
+    } catch (e) {
+        print('Error getting FCM token: $e');
+    }
+  }
 
-//     //get the FCM (Firebase Cloud Messagin) token from the device
-//     String? token = await _firebaseMessaging.getToken();
-//     print('FCM Token: $token');
-//   }
+  // Function that listens for incoming messages in the background
+  static Future<void> onBackgroundMessage(RemoteMessage message) async {
+    if (message.notification != null) {
+      print('Background message: ${message.notification!.title}');
+    }
+  }
 
-//   //funtion that will listen for incoming messages in background
-//   static Future<void> onBackgroundMessage(RemoteMessage message) async {
-//     if (message.notification != null) {
-//       print('Background message: ${message.notification!.title}');
-//     }
-//   }
+  // Handle background notification tapped (pass the payload data to the message opener screen)
+  static Future<void> onBackgroundNotificationTapped(
+      RemoteMessage message, GlobalKey<NavigatorState> navigatorKey) async {
+    navigatorKey.currentState?.pushNamed('/message', arguments: message);
+  }
 
-//   // on background notification tapped function (pass the payload data to the message opener screen)
-//   static Future<void> onBackgroundNotificationTapped(
-//       RemoteMessage message, GlobalKey<NavigatorState> navigatorKey) async {
-//     navigatorKey.currentState!.pushNamed('/message', arguments: message);
-//   }
+  // Handle foreground notifications
+  static Future<void> onForegroundNotificationTapped(
+    RemoteMessage message,
+    GlobalKey<NavigatorState> navigatorKey,
+  ) async {
+    String payloadData = jsonEncode(message.data);
 
-//   //handle foreground notifications
-//   static Future<void> onForeroundNotificationTapped(
-//     RemoteMessage message,
-//     GlobalKey<NavigatorState> navigatorKey,
-//   ) async {
-//     String payloadData = jsonEncode(message.data);
+    print("Got the message in foreground");
 
-//     print("Got the message in foreground");
+    if (message.notification != null) {
+      // Show a local notification with the payload
+      await LocalNotificationService.showInstantNotificationWithPayload(
+        title: message.notification!.title!,
+        body: message.notification!.body!,
+        payload: payloadData,
+      );
 
-//     if (message.notification != null) {
-//       await LocalNotificationService.showInstantNotificationWithPayload(
-//         title: message.notification!.title!,
-//         body: message.notification!.body!,
-//         payload: payloadData,
-//       );
-
-//       // TODO:Here to click the notification we can toggle the :onDidReceiveNotificationResponse: property in the localnotifications init function but will teach that in the lesson to avoid coflicts
-//       // here is the methode
-
-//       // on tap local notification in foreground
-//       // static void onNotificationTap(NotificationResponse notificationResponse) {
-//       //   navigatorKey.currentState!
-//       //       .pushNamed("/message", arguments: notificationResponse);
-//       // }
-
-
-
-//       //this will automatically display the payload page
-//       await navigatorKey.currentState!
-//           .pushNamed('/message', arguments: message);
-//     }
-//   }
-// }
+      // Navigate to the message page when the notification is tapped
+      await navigatorKey.currentState?.pushNamed('/message', arguments: message);
+    }
+  }
+}
